5d37682f9b790cfadc3bc96e5b2d093e9ba79c78
David Pedersen <david.pdrsn@gmail.com>
2013-12-05 13:57:18 +0100

    update example

--- examples/example.php
+++ examples/example.php
@@ -13,13 +13,10 @@
 
     <script type="text/javascript">
       $(document).ready(function() {
-        var loud = $('[data-cabanaoutloud-read-this]');
-
-        loud.cabanaOutLoud({
+        $('.controls').cabanaOutLoud({
           playText: "Læs op",
           stopText: "Stop",
-          pauseText: "Pause",
-          controlsContainerClass: 'controls'
+          pauseText: "Pause"
         });
       });
 
@@ -30,10 +27,13 @@
     <div class="controls">
     </div>
 
-<div itemprop="articleBody" data-cabanaoutloud-read-this><p>Der er nu oprettet elev- og lærlingepladser på seks ud af 10 kontrakter på byggeriet af Femern Bælt-forbindelsen, som er et af de største danske byggerier hidtil.<br class="hardreturn"><br>De fire kontrakter uden lærlinge er kontrakter om for eksempel medietræning, hvor det ikke er muligt at oprette lærepladser, oplyser Bo Eske Nielsen, koordinationsdirektør i Femern A/S.<br class="hardreturn"><br>- Der skal ansættes lærlinge på alle de områder, hvor man med rimelighed kan kræve det. Og så giver vi en økonomisk bonus, hvis entreprenøren tager flere elever på opgaven, siger Bo Eske Nielsen.<br class="hardreturn"><br>Gunde Odgaard, sekretariatschef i <span style="mso-fareast-font-family: Calibri; mso-fareast-theme-font: minor-latin;">Bygge- Anlægs- og Trækartellet (</span>BAT-kartellet) kalder Femern A/S for foregangsbygherre på området.<br class="hardreturn"><br>
-</p><h2>Femern forrest</h2>- Femern gør meget for at sikre de unge praktikpladser. Det kunne mange offentlige og private bygherrer lære af. Alle de kritiske eller dem, der mener, at klausuler er ulovlige, kan bare kigge på Femern A/S. Arbejdet, der er i gang nu, er i forbindelse med de fremrykkede aktiviteter. Det er en slags træningslejr, indtil det går rigtig løs. Det lover godt for fremtiden, siger han.<br class="hardreturn"><br>Femern A/S er lige nu i gang med fremrykkede aktiviteter til en værdi af cirka 700 millioner kroner, hvor området omkring Rødbyhavn forberedes til anlægsarbejdet.<br class="hardreturn"><br>Når der er fuldt tryk på byggeriet stiller Femern A/S krav til entreprenørerne om i alt 500 praktikpladser på milliardbyggeriet, oplyser Bo Eske Nielsen.<br class="hardreturn"><br>
-<h2>Ekstra lærling</h2>På et af byggeriets vejprojekter vader Jonas Christoffersen, der er struktørlærling hos entreprenørfirmaet MSE A/S, rundt i mudder i kulden. Men han er godt tilfreds. Især med, at Femern A/S har indføjet sociale klausuler i kontrakterne. Han var den eneste på sin årgang, der havde fået en læreplads, da han i september begyndte i MSE A/S.<br><br>&nbsp;- Det gør mig ekstra glad for at være med på projektet, som er sjovt og spændende, siger lærlingen, der forbedrer infrastrukturen i området, så det er klar til det store anlægsarbejde med at etablere den 18 kilometer lange sænketunnel mellem Rødbyhavn og Puttgarden.<br class="hardreturn"><br>
-<p></p></div>
+    <div data-cabanaoutloud-read-this>
+      En
+    </div>
+
+    <div data-cabanaoutloud-read-this>
+      To
+    </div>
 
   </body>
 </html>
