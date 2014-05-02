44283554d774cf443443951447ffd336637339e1
Leo Ørsnes <zeekooster@gmail.com>
2014-05-02 15:33:10 +0200

    Enable URL readings

--- cabana.cabanaOutLoud.parser.php
+++ cabana.cabanaOutLoud.parser.php
@@ -6,19 +6,6 @@ if (isset($_GET['parse'])) {
   header("Content-type: audio/mpeg;");
 }
 
-//$tryit = exec("runas /profile /user:admin mkdir skrdr 2>&1");
-//$tryit = exec("ffmpeg -i 2>&1");
-// $testsound = "https://translate.google.com/translate_tts?ie=UTF-8&tl=da&textlen=".strlen($splitted[$_GET['part']])."&q=".urlencode($splitted[$_GET['part']]);
-// //$tryit = exec("ffmpeg -i ".$testsound." 2>&1 | grep 'Duration'");
-// $tryit = shell_exec("ffprobe -i test.mp3 -show_format_entry duration");
-// $tryit = split("=", $tryit);
-// $duration =  floatval($tryit[1])/60;
-// $duration = number_format($duration, 2);
-// print $duration."<br />";
-// $splitduration = explode(".", $duration);
-// $minutes = explode(".", $splitduration[0]/60)[0];
-// $seconds = explode(".", $splitduration[0]/60)[1]+intval($splitduration[1]/60);
-// print $duration."<br /><br />";
 
 function multiexplode ($explodes, $string) {
   $ready = str_replace($explodes, $explodes[0], $string);
@@ -32,7 +19,7 @@ if (isset($_GET['parse'])) {
   //$page = file_get_contents("http://www.fagbladet3f.dk/2-sektion/dit-job/10f6db2a53ea47ca9b0be7d639eff4b5-20130930-her-er-de-naeste-ti-aars-megabyggerier");
   $doc = phpQuery::newDocumentHTML($page);
   foreach (pq($_GET['container']) as $item) {
-    $text .= trim(pq($item)->text()).".";
+    $text .= trim(pq($item)->text()).". ";
   }
   if (pq(".last" . $_GET['container'])) {
     $factbox = trim(pq(".last" . $_GET['container'])->text());
@@ -47,7 +34,9 @@ if (isset($_GET['parse'])) {
   $text = str_replace("..", ".", $text);
   $text = str_replace("- ", "", $text);
 
-  $splitby = Array(".", ",", "?", "!", " - ", ":");
+  $splitby = Array(". ", ".\n", ",", "?", "!", " - ", ":", "\n");
+
+  //$regex = ^(/\d*\.\d*/);
 
   $splitted = multiexplode($splitby, $text);
 
@@ -86,9 +75,22 @@ if (isset($_GET['parse'])) {
     $exsound = "https://translate.google.com/translate_tts?ie=UTF-8&tl=da&textlen=".strlen($splitted[$_GET['part']])."&q=".urlencode($splitted[$_GET['part']]);
   }
 
+  $silencepattern = "/^UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU*U$/";
+  $finaloutput = "";
+
   $getsound = file_get_contents($exsound);
   $getsound = explode("LAME3.98.2", $getsound);
-  print $getsound[0]."LAME3.98.2".$getsound[1]."LAME3.98.2".$getsound[2];
+
+
+  foreach ($getsound as $sound) {
+    if (!preg_match($silencepattern, $sound)) {
+      $finaloutput = $finaloutput.$sound;
+    }
+  }
+
+  if ($finaloutput != "") {
+    print $finaloutput;
+  }
 
 } else {
   print "Error! #887";
