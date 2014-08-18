ff61ceb434a7f0aefac44439e096acc1774c4b13
Leo Ørsnes <zeekooster@gmail.com>
2014-08-18 13:12:49 +0200

    Changed absoluteUrl

--- cabana.CabanaOutLoud.js
+++ cabana.CabanaOutLoud.js
@@ -221,8 +221,8 @@
 
       console.log("_continueOnEnd");
 
-      //var absoluteUrl = "http://cdn.cabana.dk/modules/col/v1/cabana.cabanaOutLoud.parser.php";
-      var absoluteUrl = "../cabana.cabanaOutLoud.parser.php";
+      var absoluteUrl = "http://cdn.cabana.dk/modules/col/v1/cabana.cabanaOutLoud.parser.php";
+      //var absoluteUrl = "../cabana.cabanaOutLoud.parser.php";
 
       if (typeof(Modernizr) == "undefined" ) {
 
